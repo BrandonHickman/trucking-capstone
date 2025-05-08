dc83f7275e8ad3d282bb5a9a5df480331e2f2566
Brandon Hickman <brandon_7_77@hotmail.com>
2025-05-08 15:00:42 -0500

    initial commit, adding shell modules

---
+++ components/welcome/Welcome.jsx
@@ -0,0 +1,12 @@
+import "./Welcome.css"
+
+export const Welcome = () => {
+    return (
+        <div className="welcome-container">
+            <h1><span>Welcome to</span>
+            <span>American Transport</span>
+            </h1>
+            <div>Moving America one mile at a time</div>
+        </div>
+    )
+}
